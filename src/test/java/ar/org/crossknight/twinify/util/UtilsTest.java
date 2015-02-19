2072c3d3b0c28659e47cb2445efd12d56bf62871
Julio Francisco Veronelli <julio.veronelli@crossknight.com.ar>
2015-02-19 18:26:42 -0300

    Added tests.

--- src/test/java/ar/org/crossknight/twinify/util/UtilsTest.java
+++ src/test/java/ar/org/crossknight/twinify/util/UtilsTest.java
@@ -2,15 +2,26 @@ package ar.org.crossknight.twinify.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
 
+import ar.org.crossknight.twinify.domain.delta.CreateFolderTask;
+import ar.org.crossknight.twinify.domain.delta.ExtensionFilter;
+import ar.org.crossknight.twinify.domain.delta.Filter;
+import ar.org.crossknight.twinify.domain.delta.PathFilter;
+import ar.org.crossknight.twinify.domain.delta.Task;
 import ar.org.crossknight.twinify.domain.snapshot.Folder;
+import ar.org.crossknight.twinify.domain.snapshot.Resource;
 import ar.org.crossknight.twinify.domain.snapshot.Snapshot;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class UtilsTest {
 
@@ -39,6 +50,23 @@ public class UtilsTest {
     }
 
     @Test
+    public void equalsShouldReturnTrueWhenCollectionsAreEmpty() {
+        boolean result = Utils.equals(Collections.<Resource>emptyList(), Collections.<Resource>emptyList());
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void equalsShouldReturnFalseWhenSizesAreDifferent() {
+        Collection<Resource> r1 = Arrays.asList(mock(Resource.class));
+        Collection<Resource> r2 = Arrays.asList(mock(Resource.class), mock(Resource.class));
+
+        boolean result = Utils.equals(r1, r2);
+
+        assertFalse(result);
+    }
+
+    @Test
     public void sortShouldSucceed() {
         Folder root = new Snapshot("/").getRoot();
         Folder f1 = new Folder(root, "Albany");
@@ -51,6 +79,43 @@ public class UtilsTest {
 
         assertNotSame(folders, sorted);
         assertArrayEquals(new Object[] {f3, f2, f1, f4}, sorted.toArray());
+    }
+
+    @Test
+    public void applyShouldReturnFalseWhenTasksIsEmpty() {
+        List<Task> tasks = Collections.emptyList();
+        List<Filter> filters = Arrays.<Filter>asList(new ExtensionFilter(false, "jpg"));
+
+        boolean result = Utils.apply(tasks, filters);
+
+        assertFalse(result);
+        assertTrue(tasks.isEmpty());
+    }
+
+    @Test
+    public void applyShouldReturnFalseWhenFiltersIsEmpty() {
+        Task[] allTasks = {new CreateFolderTask("/a/u")};
+        List<Task> tasks = Arrays.asList(allTasks);
+        List<Filter> filters = Collections.emptyList();
+
+        boolean result = Utils.apply(tasks, filters);
+
+        assertFalse(result);
+        assertArrayEquals(allTasks, tasks.toArray());
+    }
+
+    @Test
+    public void applyShouldSucceed() {
+        Task[] allTasks = {new CreateFolderTask("/a/u"), new CreateFolderTask("/a/v"), new CreateFolderTask("/a/w")};
+        List<Task> tasks = new ArrayList<Task>(Arrays.asList(allTasks));
+        List<Filter> filters = Arrays.<Filter>asList(new PathFilter(true, "/b/"), new PathFilter(true, "/a/u"), new PathFilter(false, "/a/v"));
+
+        boolean result = Utils.apply(tasks, filters);
+
+        assertTrue(result);
+        assertEquals(2, tasks.size());
+        assertEquals(allTasks[0], tasks.get(0));
+        assertEquals(allTasks[2], tasks.get(1));
     }
 
     @Test
